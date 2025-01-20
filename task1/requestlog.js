046b05fe05641270ef67f8e6ab13cd5060c45b45
coderamanpreet <sehgalamanpreet@yahoo.com>
2025-01-20 21:51:39 +0530

    task 1 of backend

---
+++ task1/requestlog.js
@@ -0,0 +1,4 @@
+{"timestamp":"2025-01-20T16:07:15.388Z","ip":"::ffff:127.0.0.1","url":"/","method":"GET","protocol":"http","hostname":"localhost"} 
+{"timestamp":"2025-01-20T16:07:16.623Z","ip":"::ffff:127.0.0.1","url":"/","method":"GET","protocol":"http","hostname":"localhost"}
+{"timestamp":"2025-01-20T16:08:17.338Z","ip":"::ffff:127.0.0.1","url":"/","method":"GET","protocol":"http","hostname":"localhost"}
+{"timestamp":"2025-01-20T16:17:44.149Z","ip":"::ffff:127.0.0.1","url":"/","method":"GET","protocol":"http","hostname":"localhost"}
